ffa7de064f68185f135ccfb4ffacf7ceb6968527
Matin mohammadi <matinmn87@gmail.com>
2022-05-24 14:53:30 +0430

    Fixed main method & Console.WriteLine

--- takeep.cli/Program.cs
+++ takeep.cli/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+	Console.WriteLine("Hello, takeep!");
+    }
+}
